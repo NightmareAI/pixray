0b72f1108fd382372a02d8006126e38a41af548d
palp <palp@dreamer.405rose.la>
2022-05-31 03:14:58 -0700

    workflow fixes

--- build-dreamer.sh
+++ build-dreamer.sh
@@ -2,6 +2,6 @@
 cog build -t pixray
 docker build . -t pixray-dreamer
 docker tag pixray-dreamer:latest us-docker.pkg.dev/nightmarebot-ai/nightmarebot/pixray-dreamer:latest
-docker tag pixray-dreamer:latest 438969101893.dkr.ecr.us-east-2.amazonaws.com/nightmarebot:latest
+#docker tag pixray-dreamer:latest 438969101893.dkr.ecr.us-east-2.amazonaws.com/nightmarebot:latest
 docker push us-docker.pkg.dev/nightmarebot-ai/nightmarebot/pixray-dreamer:latest
-docker push 438969101893.dkr.ecr.us-east-2.amazonaws.com/nightmarebot:latest
+#docker push 438969101893.dkr.ecr.us-east-2.amazonaws.com/nightmarebot:latest
--- pixray-workflow.py
+++ pixray-workflow.py
@@ -1,16 +1,17 @@
 import pixray
 import yaml
 import os
+import sys
 
 def main(argv):
   os.makedirs('/tmp/pixray')
-  request_settings = yaml.safe_loaf(sys.argv[0])
+  request_settings = yaml.safe_load(sys.argv[0])
   pixray.reset_settings()
   pixray.add_settings(**request_settings)
   pixray.add_settings(outdir='/tmp/pixray')
   settings = pixray.apply_settings()
   run_complete = False
-  while run_complete == False
+  while run_complete == False:
     run_complete = pixray.do_run(settings, return_display=True)
 
 if __name__ == "__main__":
